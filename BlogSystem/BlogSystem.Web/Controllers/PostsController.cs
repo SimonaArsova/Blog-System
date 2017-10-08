3561388a9e055cef3c3bd947d5fa10aae2d902e3
SimonaArsova <simona.liubomirova@gmail.com>
2017-10-08 18:09:33 +0300

    Add project

---
+++ BlogSystem/BlogSystem.Web/Controllers/PostsController.cs
@@ -0,0 +1,108 @@
+﻿using BlogSystem.Services;
+using BlogSystem.Web.Models.Posts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BlogSystem.Web.Controllers
+{
+    public class PostsController : Controller
+    {
+        private readonly IPostsService postsService;
+
+        public PostsController(IPostsService postsService)
+        {
+            this.postsService = postsService;
+        }
+
+        // GET: Posts
+        public ActionResult Index()
+        {
+            var posts = this.postsService
+                .GetAll()
+                .Select(x => new PostViewModel() {
+                    Title = x.Title,
+                    Content = x.Content,
+                    AuthorEmail = x.Author.Email,
+                    PostedOn = x.CreatedOn.Value
+                })
+                .ToList();
+
+            return View(posts);
+        }
+
+        // GET: Posts/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: Posts/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Posts/Create
+        [HttpPost]
+        public ActionResult Create(FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Posts/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: Posts/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Posts/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: Posts/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ BlogSystem/BlogSystem.Web/Models/Posts/PostViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace BlogSystem.Web.Models.Posts
+{
+    public class PostViewModel
+    {
+        public string Title { get; set; }
+
+        public string Content { get; set; }
+
+        public string AuthorEmail { get; set; }
+
+        public DateTime PostedOn { get; set; }
+    }
+}
