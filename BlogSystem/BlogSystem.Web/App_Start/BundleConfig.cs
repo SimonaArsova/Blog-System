e43f667438aaada87e9a2f624c9eb2dd4f865ab7
SimonaArsova <simona.liubomirova@gmail.com>
2017-10-11 19:43:22 +0300

    Fix login view

--- BlogSystem/BlogSystem.Web/App_Start/BundleConfig.cs
+++ BlogSystem/BlogSystem.Web/App_Start/BundleConfig.cs
@@ -26,6 +26,9 @@ namespace BlogSystem.Web
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
                       "~/Content/site.css"));
+
+            bundles.Add(new StyleBundle("~/Content/css/login").Include(
+                      "~/Content/login.css"));
         }
     }
 }
