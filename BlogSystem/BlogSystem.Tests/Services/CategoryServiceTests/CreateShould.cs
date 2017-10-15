be2d93c6678883f12fc04db7a85e5a70de3ee175
SimonaArsova <simona.liubomirova@gmail.com>
2017-10-16 01:47:46 +0300

    Test services

---
+++ BlogSystem/BlogSystem.Tests/Services/CategoryServiceTests/CreateShould.cs
@@ -0,0 +1,87 @@
+﻿using BlogSystem.Data.Contracts;
+using BlogSystem.Data.Model;
+using BlogSystem.Factories;
+using BlogSystem.Services;
+using Moq;
+using NUnit.Framework;
+using Providers.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlogSystem.Tests.Services.CategoryServiceTests
+{
+    [TestFixture]
+    public class CreateShould
+    {
+        [Test]
+        public void CallCategoryFactoryCreate()
+        {
+            // Arrange
+            var category = "category";
+            var mockedRepository = new Mock<IEfRepository<Category>>();
+            var mockedContext = new Mock<ISaveContext>();
+            var mockedGuidProvider = new Mock<IGuidProvider>();
+            var mockedCategoryFactory = new Mock<ICategoryFactory>();
+
+            var categoryService = new CategoryService(mockedRepository.Object, mockedContext.Object, mockedCategoryFactory.Object, mockedGuidProvider.Object);
+
+            //Act
+            categoryService.Create(category);
+
+            //Assert
+            mockedCategoryFactory.Verify(r => r.CreateCategory(category), Times.Once);
+        }
+
+        [Test]
+        public void CallCategoryRepoAdd()
+        {
+            // Arrange
+            var categoryName = "category";
+            var category = new Category
+            {
+                Name = categoryName
+            };
+            var mockedRepository = new Mock<IEfRepository<Category>>();
+            var mockedContext = new Mock<ISaveContext>();
+            var mockedGuidProvider = new Mock<IGuidProvider>();
+            var mockedCategoryFactory = new Mock<ICategoryFactory>();
+            mockedCategoryFactory.Setup(f => f.CreateCategory(categoryName)).Returns(category);
+
+            var categoryService = new CategoryService(mockedRepository.Object, mockedContext.Object, mockedCategoryFactory.Object, mockedGuidProvider.Object);
+
+            //Act
+            categoryService.Create(categoryName);
+
+            //Assert
+            mockedRepository.Verify(r => r.Add(category), Times.Once);
+        }
+
+        [Test]
+        public void CallContextSaveChanges()
+        {
+            // Arrange
+            var categoryName = "category";
+            var category = new Category
+            {
+                Name = categoryName
+            };
+            var mockedRepository = new Mock<IEfRepository<Category>>();
+            var mockedContext = new Mock<ISaveContext>();
+            var mockedGuidProvider = new Mock<IGuidProvider>();
+            var mockedCategoryFactory = new Mock<ICategoryFactory>();
+            mockedCategoryFactory.Setup(f => f.CreateCategory(categoryName)).Returns(category);
+
+            var categoryService = new CategoryService(mockedRepository.Object, mockedContext.Object, mockedCategoryFactory.Object, mockedGuidProvider.Object);
+
+            //Act
+            categoryService.Create(categoryName);
+
+            //Assert
+            mockedContext.Verify(r => r.SaveChanges(), Times.Once);
+        }
+
+    }
+}
---
+++ BlogSystem/BlogSystem.Tests/Services/PostsServiceTests/UpdateShould.cs
@@ -0,0 +1,140 @@
+﻿using BlogSystem.Data.Contracts;
+using BlogSystem.Data.Model;
+using BlogSystem.Factories;
+using BlogSystem.Services;
+using BlogSystem.Services.Contracts;
+using Moq;
+using NUnit.Framework;
+using Providers.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlogSystem.Tests.Services.PostsServiceTests
+{
+    [TestFixture]
+    public class UpdateShould
+    {
+        [Test]
+        public void CallRepositoryAll()
+        {
+            var id = Guid.NewGuid();
+            var user = new Mock<User>();
+            var category = new Mock<Category>();
+            var post = new Post
+            {
+                Id = id,
+                Author = user.Object,
+                Category = category.Object
+            };
+
+            var posts = new List<Post> { post }.AsQueryable();
+
+            var mockedRepository = new Mock<IEfRepository<Post>>();
+            mockedRepository.Setup(r => r.All).Returns(posts);
+            var mockedContext = new Mock<ISaveContext>();
+            var mockedUserService = new Mock<IUserService>();
+            var mockedCategoryService = new Mock<ICategoryService>();
+            var mockedPostFactory = new Mock<IPostFactory>();
+            var mockedDateTimeProvider = new Mock<IDateTimeProvider>();
+            var mockedGuidProvider = new Mock<IGuidProvider>();
+
+            var service = new PostsService
+            (
+                mockedRepository.Object,
+                mockedPostFactory.Object,
+                mockedContext.Object,
+                mockedUserService.Object,
+                mockedCategoryService.Object,
+                mockedDateTimeProvider.Object,
+                mockedGuidProvider.Object
+             );
+
+            service.Update(id, "title", "content", "image");
+
+            mockedRepository.Verify(r => r.All, Times.Once);
+        }
+
+        [Test]
+        public void CallRepositoryUpdate()
+        {
+            var id = Guid.NewGuid();
+            var user = new Mock<User>();
+            var category = new Mock<Category>();
+            var post = new Post
+            {
+                Id = id,
+                Author = user.Object,
+                Category = category.Object
+            };
+
+            var posts = new List<Post> { post }.AsQueryable();
+
+            var mockedRepository = new Mock<IEfRepository<Post>>();
+            mockedRepository.Setup(r => r.All).Returns(posts);
+            var mockedContext = new Mock<ISaveContext>();
+            var mockedUserService = new Mock<IUserService>();
+            var mockedCategoryService = new Mock<ICategoryService>();
+            var mockedPostFactory = new Mock<IPostFactory>();
+            var mockedDateTimeProvider = new Mock<IDateTimeProvider>();
+            var mockedGuidProvider = new Mock<IGuidProvider>();
+
+            var service = new PostsService
+            (
+                mockedRepository.Object,
+                mockedPostFactory.Object,
+                mockedContext.Object,
+                mockedUserService.Object,
+                mockedCategoryService.Object,
+                mockedDateTimeProvider.Object,
+                mockedGuidProvider.Object
+             );
+
+            service.Update(id, "title", "content", "image");
+
+            mockedRepository.Verify(r => r.Update(post), Times.Once);
+        }
+
+        [Test]
+        public void CallContextSaveChanges()
+        {
+            var id = Guid.NewGuid();
+            var user = new Mock<User>();
+            var category = new Mock<Category>();
+            var post = new Post
+            {
+                Id = id,
+                Author = user.Object,
+                Category = category.Object
+            };
+
+            var posts = new List<Post> { post }.AsQueryable();
+
+            var mockedRepository = new Mock<IEfRepository<Post>>();
+            mockedRepository.Setup(r => r.All).Returns(posts);
+            var mockedContext = new Mock<ISaveContext>();
+            var mockedUserService = new Mock<IUserService>();
+            var mockedCategoryService = new Mock<ICategoryService>();
+            var mockedPostFactory = new Mock<IPostFactory>();
+            var mockedDateTimeProvider = new Mock<IDateTimeProvider>();
+            var mockedGuidProvider = new Mock<IGuidProvider>();
+
+            var service = new PostsService
+            (
+                mockedRepository.Object,
+                mockedPostFactory.Object,
+                mockedContext.Object,
+                mockedUserService.Object,
+                mockedCategoryService.Object,
+                mockedDateTimeProvider.Object,
+                mockedGuidProvider.Object
+             );
+
+            service.Update(id, "title", "content", "image");
+
+            mockedContext.Verify(r => r.SaveChanges(), Times.Once);
+        }
+    }
+}
